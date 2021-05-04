56b242112e6ce50a21f517dc7402c3a64b958863
miki kimura <35397855+mikipon@users.noreply.github.com>
2021-05-04 17:18:24 +0900

    初めてのGitHub

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //回転速さ
+    public float turnSpeed = 20f;
+
+    Animator m_Animator;
+    Rigidbody m_Rigidbody;
+    Vector3 m_Movement;//メンバー変数（命令規則）（この変数名のつけ方をパスカルケースと呼ぶ）
+    Quaternion m_Rotation = Quaternion.identity;//デフォルト
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // 一定秒数ごとによばれる
+    void FixedUpdate()
+    {
+        //horizontal(水平)とvertical（垂直）の値を取得
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        //ピタゴラスの定理では一番長い線が1.414であるため
+        //斜めのほうが早く移動してしまうため、正規化する
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize();
+
+        //浮動小数点を比較し、近似している場合はFalse
+        //水平変数がほぼゼロの場合、メソッドはTrueを返す
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);//横軸
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);//縦軸
+        bool isWalking = hasHorizontalInput || hasVerticalInput; //hasHorizo​​ntalInputまたはhasVerticalInputがtrueの場合、isWalkingはtrue
+
+        //アニメーションをセット
+        m_Animator.SetBool("IsWalking", isWalking);
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+
+    }
+
+    private void OnAnimatorMove()
+    {
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
+        m_Rigidbody.MoveRotation(m_Rotation);
+    }
+}
